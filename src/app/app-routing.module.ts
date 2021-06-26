abdbeeedba0b2a889534a38e2ddf773771384340
engZena <zena_al_ja95@yahoo.com>
2021-06-26 18:51:26 +0300

    complete-routing-add-nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FruitsStoreComponent } from './containers/fruits-store/fruits-store.component';
+import { LoginComponent } from './containers/login/login.component';
+import { SignupComponent } from './containers/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FruitsStoreComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/containers/login/login.component.ts
+++ src/app/containers/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
     @ViewChild('loginForm', { static: false }) loginForm: NgForm;
+
+    constructor( private router:Router){
+
+    }
     onSubmit(form: NgForm) {
     console.log(form);
+  }
+
+  onSignUp(){
+    this.router.navigate(['/signup']);
   }
 }
