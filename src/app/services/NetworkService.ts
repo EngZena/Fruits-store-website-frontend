dcdd0f5f8cd806a6fd2b9d9ab8f66aa8d015ba75
engZena <zena_al_ja95@yahoo.com>
2022-02-25 19:01:49 +0300

    add-no-internet-page

---
+++ src/app/services/NetworkService.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { fromEvent, merge, Observable, Observer } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NetworkService {
+    createOnline$() {
+        return merge<boolean>(
+          fromEvent(window, 'offline').pipe(map(() => false)),
+          fromEvent(window, 'online').pipe(map(() => true)),
+          new Observable((sub: Observer<boolean>) => {
+            sub.next(navigator.onLine);
+            sub.complete();
+          }));
+      }
+}
