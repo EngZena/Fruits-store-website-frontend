aada4758d3f60826c5cb494f648f5197f5208198
engZena <zena_al_ja95@yahoo.com>
2022-05-01 03:16:49 +0300

    import-AdminSharedComponentsModule-in-admin-containers-module

--- src/app/admin-site/containers/admin.containers.module.ts
+++ src/app/admin-site/containers/admin.containers.module.ts
@@ -1,3 +1,4 @@
+import { AdminSharedComponentsModule } from '../shared/admin.shared.components.module';
 import { AdminSiteHomePageComponent } from './admin-site-home-page/admin-site-home-page.component';
 import { CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers/customers.component';
@@ -6,7 +7,7 @@ import { NgModule } from '@angular/core';
 const createdComponents = [AdminSiteHomePageComponent, CustomersComponent];
 @NgModule({
   declarations: [...createdComponents],
-  imports: [CommonModule],
+  imports: [CommonModule, AdminSharedComponentsModule],
   exports: [...createdComponents],
 })
 export class AdminContainersModule {}
